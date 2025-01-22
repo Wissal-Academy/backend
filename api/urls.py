5c69f3999c5d0555606183cff54618a3625cc84c
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2025-01-22 11:39:52 +0100

    Add views to api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+from .views import ItemViewSet, LoginView, RegisterView
 
 router = DefaultRouter()
 router.register('items', ItemViewSet)
 
 urlpatterns = [
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
     path('', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,43 @@
-from rest_framework import viewsets
-from .serializers import ItemSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ItemSerializer, LoginSerializer, UserSerializer
 from .models import Item
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
